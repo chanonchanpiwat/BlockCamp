2b86bbf0e21f490bcbb591d7521d2620ccfed69b
chanon.chanpiwat@gmail.com <chanon.chanpiwat@gmail.com>
2022-08-03 19:16:32 +0700

    ten

--- contracts/SCAD.sol
+++ contracts/SCAD.sol
@@ -10,14 +10,13 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 contract SCAD is AccessControl {
 
     event Propose(address indexed sender, uint proposalId);
-    event Approve(address indexed sender,uint proposalId,uint amount);
-    event Refund(address indexed sender,uint proposalId,uint amount);
+    event Approve(address indexed sender, uint proposalId);
+    event Refund(address indexed sender, uint amount);
     event Reward(address[] indexed inspector, uint proposalId, uint reward);
 
     IERC20 token;
     mapping(address => uint) balanceOf;
     mapping(uint => Proposal) proposalQuene;
-    bytes32 public constant PROPOSER_ROLE = keccak256("PROPOSER");
     bytes32 public constant APPROVER_ROLE = keccak256("APPROVER");
 
     enum Status {
@@ -28,68 +27,54 @@ contract SCAD is AccessControl {
 
     struct Proposal {
         address proposer;
-        bytes data;
+        string data;
         uint start;
         uint period;
         Status state;
         uint bounty;
     }
 
-    constructor(address[] memory proposer, address[] memory approver, address _token) {
+    constructor(address[] memory approver, address _token) {
         token = IERC20(_token);
-        for (uint256 i = 0; i < proposer.length; ++i) {
-            _setupRole(PROPOSER_ROLE, proposer[i]);
-        }
-
         for (uint256 i = 0; i < approver.length; ++i) {
-            _setupRole(PROPOSER_ROLE, approver[i]);
+            _setupRole(APPROVER_ROLE, approver[i]);
         }
     }
-
-
-    modifier onlyProposer {
-      require(hasRole(PROPOSER_ROLE, msg.sender),"Proposer role is required");
-      _;
-   }
 
    modifier onlyApprover {
       require(hasRole(APPROVER_ROLE, msg.sender),"Approver role is required");
       _;
    }
 
+
     function _deposit(uint _amount) internal {
         require(token.balanceOf(msg.sender) >= _amount, "Insufficient token balance");
         require(token.allowance(msg.sender ,address(this)) >= _amount,"Contract must be approved");
         token.transferFrom(msg.sender, address(this), _amount);
-        
-        
     }
 
-    function propose(address client, string memory func, uint _period, uint _amount) external onlyProposer {
-        bytes memory _data = abi.encodePacked(func);
-        uint proposalId = uint(keccak256(abi.encodePacked(client,_data,_period)));
+    function propose(string memory contractName, uint _period, uint _amount) external returns(uint)  {
+        uint proposalId = uint(keccak256(abi.encodePacked(msg.sender,contractName,_period)));
+        _deposit(_amount);
         proposalQuene[proposalId] = Proposal({
-            proposer: client,
-            data: _data,
+            proposer: msg.sender,
+            data: contractName,
             start: block.timestamp,
             period: _period,
             state: Status.pending,
             bounty: _amount
         });
         emit Propose(msg.sender,proposalId);
-
+        return proposalId;
     }
 
 
-    function approve(uint proposalId, uint _amount) external {
+    function approve(uint proposalId) external onlyApprover {
         Proposal storage proposal = proposalQuene[proposalId];
         require(proposal.proposer == msg.sender, "Can only approved your proposal");
-        _deposit(proposal.bounty);
         proposal = proposalQuene[proposalId];
-        proposal.bounty = _amount;
-        proposal.start = block.timestamp;
         proposal.state = Status.aprroved;
-        emit Approve(msg.sender, proposalId, _amount);
+        emit Approve(msg.sender, proposalId);
     }
 
 
@@ -109,13 +94,25 @@ contract SCAD is AccessControl {
         token.transfer(msg.sender, _amount);
     }
 
+    function _refund(uint _amount) internal {
+        token.transfer(msg.sender, _amount);
+        emit Refund(msg.sender, _amount);
+
+    }
+
     function refund(uint proposalId) external {
         Proposal storage proposal = proposalQuene[proposalId];
-        require(proposal.proposer == msg.sender, "Can only approved your proposal");
-        require(proposal.start + proposal.period > block.timestamp,"Your contract are under audited");
-        token.transfer(msg.sender, proposal.bounty);
-        emit Refund(msg.sender, proposalId, proposal.bounty);
-
+        require(proposal.proposer == msg.sender, "can withdraw only your deposition");
+        uint amount = proposal.bounty;
+        if (proposal.state == Status.rejected) {
+            _refund(amount);
+        } else if (proposal.start + proposal.period > block.timestamp) {
+            _refund(amount);
+        } else if (proposal.start + 2 > block.timestamp) {
+            _refund(amount);
+        } else {
+            revert("contract are under audited process");
+        }
     }
 
 }
